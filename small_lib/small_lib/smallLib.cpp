53fb4a3a0b942544194ecf506dea04e8820d3c09
MaminGansta <sa00709@mail.ru>
2020-02-26 13:03:35 +0300

    small lib vec

--- small_lib/small_lib/smallLib.cpp
+++ small_lib/small_lib/smallLib.cpp
@@ -1,16 +1,124 @@
+#include <cstdlib>
+#include <cassert>
+#include <memory.h>
+
+#ifndef MAX
+#define MAX(a,b) (a > b? a : b)
+#endif
 
 namespace small
-
 {
+
 	template <typename T, size_t capacity>
 	struct array
 	{
-		T data;
+		T data[capacity];
 		size_t size;
 
-		array() : size(0) { data = new T[capacity]; }
+		array() : size(0) {}
 
+		T& operator [] (int idx)
+		{
+			assert((unsigned)idx < size);
+			return data[idx];
+		}
+
+		void push_back(const T&& elem)
+		{
+			assert(size <= capacity);
+			data[size++] = elem;
+		}
+
+		T& pop()
+		{
+			return data[--size];
+		}
 	};
+
+	template <typename T>
+	struct vector
+	{
+		T* data;
+		size_t capacity;
+		size_t size;
+
+		vector() : data(nullptr), size(0), capacity(0) {}
+		vector(int capacity) : data(nullptr), size(0), capacity(capacity)
+		{
+
+		}
+
+		vector(size_t size, T const& value = T{}) : data(nullptr), capacity(size)
+		{
+			grow(capacity);
+			for (int i = 0; i < size; i++)
+				push_back(value);
+		}
+
+		~vector()
+		{
+			for (int i = 0; i < size; i++)
+				data[i].~T();
+			::operator delete(data);
+		}
+
+		void push_back(const T && value)
+		{
+			size_t new__size = size + 1;
+			if (new__size > capacity) 
+				grow(new__size);
+			new(data + size) T(value);
+			size = new__size;
+		}
+
+		T& operator [](int i)
+		{
+			assert((unsigned)i < size);
+			return data[i];
+		}
+
+		void erise(int i)
+		{
+			assert((unsigned)i < size);
+			data[i].~T();
+			memmove(data + i, data + i + 1, sizeof(T) * (size - i));
+		}
+
+		void reserve(size_t reserve_size)
+		{
+			reserve_mem(reserve_size);
+		}
+
+		T* begin() { return data; }
+		T* end() { return data + size; }
+
+	private:
+
+		void grow(size_t new_cap)
+		{
+			new_cap = MAX(2 * capacity + 1, new_cap);
+			assert(new_cap > capacity);
+			T* new_buffer = static_cast<T*>(::operator new(new_cap * sizeof(T)));
+
+			// this shit must be tested
+			//if (data)
+			//{
+				assert(data == nullptr && size != 0);
+				memmove(new_buffer, data, sizeof(T) * size);
+				::operator delete(data);
+			//}
+			data = new_buffer;
+			assert(data);
+			capacity = new_cap;
+		}
+
+		void reserve_mem(size_t new_cap)
+		{
+
+		}
+	};
+
+
 
 
 }
--- small_lib/small_lib/test.cpp
+++ small_lib/small_lib/test.cpp
@@ -1,7 +1,21 @@
 
+#include "smallLib.cpp"
 
 int main(void)
 {
+	
+
+	using namespace small;
+
+	array<int, 2> a;
+
+	a.push_back(3);
+	a.push_back(4);
+
+
+	a[0] = 1;
+	a[1] = 2;
+
 
 	return 0;
 }
