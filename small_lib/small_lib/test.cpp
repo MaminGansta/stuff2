3d94df947b6d10767d1d2f1cc8eb01fb46253073
mamingansta <sa00709@mail.ru>
2020-02-29 16:52:28 +0300

    small lib hash table

--- small_lib/small_lib/test.cpp
+++ small_lib/small_lib/test.cpp
@@ -5,6 +5,11 @@
 #include <utility>
 #include <vector>
 
+
+#include <unordered_map>
+
+
+
 struct mystruct
 {
 	int a;
@@ -44,9 +49,29 @@ int main(void)
 
 	using namespace small;
 
-	smallvector<mystruct> a(3);
+	hashmap<int, int, 2> map;
+	map[1] = 2;
 
-	//vector<mystruct> b(3, mystruct());
+
+	hashmap<std::string, int, 2> mapp;
+
+	mapp["dsf"] = 3;
+	mapp["dsf"] = 3;
+	mapp["fdsf"] = 21;
+	mapp["dsdfsf"] = 1;
+	mapp["dsasdff"] = 31;
+
+	mapp.add("dsf", 3);
+
+	mapp.has("dsf");
+	mapp.remove("dsf");
+	mapp.has("dsf");
+
+
+
+	//vector<std::pair<int, int>> container[2];
+	//container[0].push_back(std::make_pair(1, 2));
+
 
 	return 0;
 }
